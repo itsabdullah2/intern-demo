65f35951ccf524eb64fe111718055c7a0e1c36df
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-29 19:38:59 +0300

    everything except functionality

--- src/components/AddBtn.tsx
+++ src/components/AddBtn.tsx
@@ -1,5 +1,12 @@
+import { LuPlus } from "react-icons/lu";
+
 const AddBtn = () => {
-  return <div>AddBtn</div>;
+  return (
+    <button className="flex items-center gap-2 rounded-full bg-blue py-3 px-5 text-white font-medium hover:bg-dark-blue duration-200 cursor-pointer active:scale-95">
+      <LuPlus />
+      Add
+    </button>
+  );
 };
 
 export default AddBtn;
