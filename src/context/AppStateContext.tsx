adc6cda36fc0ce8f00659263fb94cfbeb47fa931
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-29 19:37:09 +0300

    generate new states & functions

--- src/context/AppStateContext.tsx
+++ src/context/AppStateContext.tsx
@@ -1,9 +1,6 @@
 import { createContext, useState } from "react";
-
-interface AppStateContextType {
-  isDarkMode: boolean;
-  handleThemeToggle: () => void;
-}
+import { AppStateContextType, NoteItemType } from "../types";
+import { notesData } from "../components/NotesList";
 
 export const AppStateContext = createContext<AppStateContextType | null>(null);
 
@@ -13,15 +10,33 @@ export const AppStateProvider = ({
   children: React.ReactNode;
 }) => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [activeCategory, setActiveCategory] = useState<string>("all");
+  // const [notesArray, setNotesArray] = useState<NoteItemType[]>([]);
+  const [completedNotesIds, setCompletedNotesIds] = useState<string[]>([]);
+  const [showOnlyCompleted, setShowOnlyCompleted] = useState<boolean>(false);
 
   const handleThemeToggle = () => {
     setIsDarkMode((prev) => !prev);
   };
 
+  // Inputs function handlers
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
   const values: AppStateContextType = {
     isDarkMode,
+    searchValue,
+    activeCategory,
+    completedNotesIds,
+    showOnlyCompleted,
     // Functions
     handleThemeToggle,
+    handleSearchChange,
+    setActiveCategory,
+    setCompletedNotesIds,
+    setShowOnlyCompleted,
   };
 
   return (
