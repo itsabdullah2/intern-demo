2c18f356a64e5f73e9e925fd73b6e5494f073b6f
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-30 12:42:14 +0300

    pass the data to NoteItem component, finish search functionality, & sort the data by their date & render default illustration

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,64 +1,59 @@
 import useAppState from "../context/useAppState";
-import { NoteItemType } from "../types";
 import NoteItem from "./NoteItem";
-
-interface Note extends NoteItemType {
-  id: string;
-}
-
-export const notesData: Note[] = [
-  {
-    id: "1",
-    title: "Design mockups",
-    category: "business",
-    description: "note description",
-  },
-  {
-    id: "2",
-    title: "React component",
-    category: "personal",
-    description: "note description",
-  },
-  {
-    id: "3",
-    title: "Color scheme",
-    category: "home",
-    description: "note description",
-  },
-  {
-    id: "4",
-    title: "Deployment script",
-    category: "business",
-    description: "note description",
-  },
-];
+import notFound from "../assets/not-found.svg";
 
 const NotesList = () => {
-  const { activeCategory, completedNotesIds, showOnlyCompleted } =
+  const { activeCategory, showOnlyCompleted, notes, searchValue } =
     useAppState();
 
   let filteredNotes =
     activeCategory === "all"
-      ? notesData
-      : notesData.filter((note) => note.category == activeCategory);
+      ? notes
+      : notes.filter((note) => note.category == activeCategory);
 
   if (showOnlyCompleted) {
-    filteredNotes = filteredNotes.filter((note) =>
-      completedNotesIds.includes(note.id)
-    );
+    filteredNotes = filteredNotes.filter((note) => note.checked);
+  } else {
+    filteredNotes = [
+      // Reorder the notes
+      ...filteredNotes.filter((note) => !note.checked),
+      ...filteredNotes.filter((note) => note.checked),
+    ];
   }
 
-  return (
+  if (searchValue.trim()) {
+    const lowerSearch = searchValue.toLowerCase();
+
+    filteredNotes = filteredNotes.filter((note) =>
+      note.title.toLowerCase().includes(lowerSearch)
+    );
+  }
+  // Sort the notes by their date
+  filteredNotes.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+
+  return filteredNotes.length > 0 ? (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 lg:gap-8">
       {filteredNotes.map((note) => (
         <NoteItem
           key={note.id}
           id={note.id}
+          checked={note.checked}
           category={note.category}
           title={note.title}
           description={note.description}
+          createdAt={note.createdAt}
         />
       ))}
+    </div>
+  ) : (
+    <div className="w-96 mx-auto opacity-50">
+      <img
+        src={notFound}
+        alt="Searching Illustration"
+        className="w-full h-full"
+      />
     </div>
   );
 };
