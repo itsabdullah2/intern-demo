bf25309c067cdb98771dba301abcfcdc68d0da87
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-30 12:38:41 +0300

    finish button functionality

--- src/components/AddBtn.tsx
+++ src/components/AddBtn.tsx
@@ -1,8 +1,14 @@
 import { LuPlus } from "react-icons/lu";
+import useAppState from "../context/useAppState";
 
 const AddBtn = () => {
+  const { handlePopup } = useAppState();
+
   return (
-    <button className="flex items-center gap-2 rounded-full bg-blue py-3 px-5 text-white font-medium hover:bg-dark-blue duration-200 cursor-pointer active:scale-95">
+    <button
+      className="flex items-center gap-2 rounded-full bg-blue py-3 px-5 text-white font-medium hover:bg-dark-blue duration-200 cursor-pointer active:scale-95"
+      onClick={handlePopup}
+    >
       <LuPlus />
       Add
     </button>
